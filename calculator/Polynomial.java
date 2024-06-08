425c4ca89f5e41b95dc44ca78c779e932d1c0611
ranbrabgu <ranbra@post.bgu.ac.il>
2024-06-08 10:53:22 +0300

    oah yaaa

---
+++ calculator/Polynomial.java
@@ -0,0 +1,93 @@
+package calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Polynomial{
+
+    private List<Monomial> monomials;
+
+
+    public Polynomial(){ 
+        this.monomials= new ArrayList<>();
+    }
+
+
+    public static Polynomial build(String input){
+        Polynomial poly= new Polynomial();
+        int exponent=0;
+        String[] coeffs = input.split("\\s+");
+        for(String coeff : coeffs){
+            poly.monomials.add(new Monomial(ParseScalar(coeff), exponent));
+            exponent++;
+        }
+        return poly;
+    }
+
+    private static Scalar ParseScalar(String num){
+        if(num.contains("/")){
+            String[] ratio= num.split(num);
+            return new RationalScalar(Integer.parseInt(ratio[0]), Integer.parseInt(ratio[1]));
+        }
+        return new IntegerScalar(Integer.parseInt(num));
+    }
+
+
+    public Polynomial add(Polynomial p){
+        Polynomial result= new Polynomial();
+        for(int i=0; i<= monomials.size(); i++){
+            Scalar coeff= (this.monomials.get(i).getCoefficient()).add(p.monomials.get(i).getCoefficient());
+            result.monomials.add(new Monomial(coeff, i));
+        }
+        return result;
+    }
+
+
+    public Polynomial mul(Polynomial p){
+        Polynomial result= new Polynomial();
+        for (Monomial m1 : this.monomials) {
+            for (Monomial m2 : p.monomials) {
+                Scalar coeff = m1.getCoefficient().mul(m2.getCoefficient());
+                Polynomial adding = new Polynomial();
+                adding.monomials.add(new Monomial(coeff, m1.getExponent()+m2.getExponent()));
+                result.add(adding);
+            }
+        }
+        return result;
+    }
+
+
+    public Scalar evaluate(Scalar s){
+        Scalar res= new IntegerScalar(0);
+        for(Monomial m : this.monomials){
+            res.add(m.getCoefficient().mul((s.power(m.getExponent()))));
+        }
+        return res;
+    }
+
+
+    public Polynomial derivative(Polynomial p){
+        Polynomial res= new Polynomial();
+        for (Monomial m : p.monomials) {
+            res.monomials.add(m.derivative());
+        }
+        return res;
+    }
+
+
+    public boolean equals(Object o){
+        if(!(o instanceof Polynomial)){
+            return false;
+        }
+        return monomials.equals(((Polynomial)o).monomials);
+    }
+
+
+    public String toString(){
+        String res= "";
+        for(Monomial m : this.monomials){
+            res= res + m.toString();
+        }
+        return res;
+    }
+}
---
+++ calculator/testing.java
@@ -0,0 +1,51 @@
+package calculator;
+
+public class testing {
+    public static void main(String[] args) {
+        System.out.println("in main");
+
+        
+
+        // ---evaluating valuse---
+        // [1,5,2/3,8,4/3,4/3,28,1/1,4/4]
+        // ---Monomials---
+        // 1: int+ ^ int
+        // 2: int- ^ int
+        // 3: int+ ^ 0
+        // 4: int- ^ 0 
+        // 5: rational+ ^ int a
+        // 6: rational+ ^ int b
+        // 7: rational- ^ int a
+        // 8: rational- ^ int b
+        // 9: rational+ ^ 0
+        // 10: rational- ^ 0
+
+        //---cheacks--
+        // n + n
+        // n * n
+        //  
+        // for each output:
+        //      sign()
+        //
+        //
+        //---Polynomials---
+        // create 10 comibations with monomials
+        // n + n
+        // n * n
+        // 
+        //  for each: 
+        //      deriveative
+        //      evaluate
+        //  
+          
+        
+        
+
+
+
+
+
+
+    }
+    
+}
